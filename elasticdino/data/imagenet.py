1c4f016586e37100586501e5b864d8a6bdc13840
Ulysse Mizrahi <odusseys@gmail.com>
2025-01-18 18:57:44 +0200

    fix imagenet

--- elasticdino/data/imagenet.py
+++ elasticdino/data/imagenet.py
@@ -3,6 +3,8 @@ from datasets import  VerificationMode, load_dataset
 from torch.utils.data import DataLoader
 import torchvision
 import os
+import torch
+
 
 NUM_WORKERS = int(os.environ.get("NUM_WORKERS", 1))
 
@@ -16,6 +18,7 @@ imagenet = load_dataset("timm/imagenet-22k-wds",
                         num_proc=NUM_WORKERS)
 
 
+
 def process(sample):
     img = sample["jpg"].convert("RGB")
     l = min(img.width, img.height)
@@ -23,6 +26,9 @@ def process(sample):
     img = img.resize((256, 256))
     return torchvision.transforms.functional.pil_to_tensor(img) / 255.0
 
+def collate_fn(samples):
+   return torch.stack([process(s) for s in samples])
+
 def load_imagenet(batch_size):
-  return DataLoader(imagenet, batch_size=batch_size, shuffle=True, pin_memory=True, num_workers=NUM_WORKERS)
+  return DataLoader(imagenet, batch_size=batch_size, collate_fn=collate_fn, shuffle=True, pin_memory=True, num_workers=NUM_WORKERS)
   
